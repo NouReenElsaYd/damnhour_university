12356f01c3812015d54cfeee98397c1d3d8bd080
Abdoshadwan <abdomen511@gmail.com>
2025-03-27 17:10:22 +0200

    C

--- lib/layout/layout.dart
+++ lib/layout/layout.dart
@@ -15,78 +15,78 @@ class LayoutScreen extends StatelessWidget {
       listener: (BuildContext context, Object? state) {},
       builder:
           (BuildContext context, state) => Scaffold(
-        backgroundColor: Colors.white,
-        //appBar: AppBar(),
-        body: cubit.screens[cubit.currentIndex],
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(  // for making elevation that is in the bottomNavBat
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey, // Stronger shadow
-                blurRadius: 13, //  more spread shadow
-                spreadRadius: -10, // Makes shadow more visible
-              ),
-            ],
-          ),
-          child: BottomNavigationBar(
-            currentIndex: cubit.currentIndex,
-            onTap: (index) {
-
-              cubit.changeBottomNav(index);
-            },
-            items: [
-              _buildNavItem(Icons.person_2_outlined, 'الحساب', 0, cubit),
-              _buildNavItem(Icons.history, 'السجل', 1, cubit),
-              _buildNavItem(Icons.article_outlined, 'الشكاوي', 2, cubit),
-              _buildNavItem(Icons.home_outlined, 'الرئيسية', 3, cubit),
-            ],
-            selectedItemColor: primary_blue,
-            unselectedItemColor: primary_blue,
             backgroundColor: Colors.white,
-            type: BottomNavigationBarType.fixed,
-            selectedLabelStyle: TextStyle(
-              fontFamily: 'Cairo',
-              fontSize: 14,
-              fontWeight: FontWeight.w600,
-            ),
-            unselectedLabelStyle: TextStyle(
-              fontFamily: 'Cairo',
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
+            //appBar: AppBar(),
+            body: cubit.screens[cubit.currentIndex],
+            bottomNavigationBar: Container(
+              decoration: BoxDecoration(
+                // for making elevation that is in the bottomNavBat
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey, // Stronger shadow
+                    blurRadius: 13, //  more spread shadow
+                    spreadRadius: -10, // Makes shadow more visible
+                  ),
+                ],
+              ),
+              child: BottomNavigationBar(
+                currentIndex: cubit.currentIndex,
+                onTap: (index) {
+                  cubit.changeBottomNav(index);
+                },
+                items: [
+                  _buildNavItem(Icons.person_2_outlined, 'الحساب', 0, cubit),
+                  _buildNavItem(Icons.history, 'السجل', 1, cubit),
+                  _buildNavItem(Icons.article_outlined, 'الشكاوي', 2, cubit),
+                  _buildNavItem(Icons.home_outlined, 'الرئيسية', 3, cubit),
+                ],
+                selectedItemColor: primary_blue,
+                unselectedItemColor: primary_blue,
+                backgroundColor: Colors.white,
+                type: BottomNavigationBarType.fixed,
+                selectedLabelStyle: TextStyle(
+                  fontFamily: 'Cairo',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                ),
+                unselectedLabelStyle: TextStyle(
+                  fontFamily: 'Cairo',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
             ),
           ),
-        ),
-      ),
     );
   }
 
   BottomNavigationBarItem _buildNavItem(
-      IconData icon,
-      String label,
-      int index,
-      UniversityCubit cubit,
-      ) {
+    IconData icon,
+    String label,
+    int index,
+    UniversityCubit cubit,
+  ) {
     bool isSelected = index == cubit.currentIndex;
 
     return BottomNavigationBarItem(
-      icon: isSelected
-          ? Container(
-        width: ScreenSize.width * 0.16,
-        height: ScreenSize.height * 0.045,
-        padding: EdgeInsets.symmetric(
-          vertical: ScreenSize.height * 0.01,
-          horizontal: ScreenSize.width * 0.04,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8.0),
-          color: brandColor25,
-        ),
-        child: Icon(icon, color: primary_blue),
-      )
-          : Icon(icon, color: primary_blue),
+      icon:
+          isSelected
+              ? Container(
+                width: ScreenSize.width * 0.16,
+                height: ScreenSize.height * 0.045,
+                padding: EdgeInsets.symmetric(
+                  vertical: ScreenSize.height * 0.01,
+                  horizontal: ScreenSize.width * 0.04,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: brandColor25,
+                ),
+                child: Icon(icon, color: primary_blue),
+              )
+              : Icon(icon, color: primary_blue),
       label: label, // if i put any label the string will duplicate
     );
   }
-
 }
--- lib/modules/complaints/complaints.dart
+++ lib/modules/complaints/complaints.dart
@@ -1,4 +1,6 @@
-
+import 'package:damnhour_university/shared/components/components.dart';
+import 'package:damnhour_university/shared/constants/constants.dart';
+import 'package:damnhour_university/shared/styles/colors.dart';
 import 'package:flutter/material.dart';
 
 class ComplaintsScreen extends StatelessWidget {
@@ -6,9 +8,82 @@ class ComplaintsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('Complaints'),
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Padding(
+            padding: EdgeInsetsDirectional.only(
+              top: ScreenSize.height * 0.055,
+              start: ScreenSize.width * 0.04,
+              end: ScreenSize.width * 0.04,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: accentColor50,
+                        shape: BoxShape.circle,
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.notifications_none,
+                          color: primary_blue,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        // mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          TextCairo(
+                            text: 'جامعة دمنهور',
+                            color: accent_orange,
+                            fontweight: FontWeight.w400,
+                            fontsize: 13.0,
+                          ),
+                          TextCairo(
+                            text: 'الشكاوي والمقترحات',
+                            color: Colors.black,
+                            fontweight: FontWeight.w500,
+                            fontsize: 16.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 15.0),
+                    Container(
+                      height: 52,
+                      child: Image(image: AssetImage("assets/images/logo.png")),
+                    ),
+                  ],
+                ),
+                SizedBox(height: ScreenSize.height * .02),
+                TextCairo(
+                  text: 'أبرز الشكاوي والاقتراحات',
+                  color: Colors.black,
+                  fontsize: 18.0,
+                  fontweight: FontWeight.w500,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Padding(
+            padding: EdgeInsetsDirectional.only(
+              end: ScreenSize.width * 0.04,
+              bottom: ScreenSize.height * 0.02,
+            ),
+            child: sectorsListView(),
+          ),
+          Container(height: 1.0, color: brandColor25),
+        ],
       ),
     );
   }
