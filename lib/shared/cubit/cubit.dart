f9701e85757f0685e26bb6a45ccef61f0d3797da
Noureen <noureenelsayd@gmail.com>
2025-04-16 18:50:37 +0200

    protection safety screen

--- lib/shared/cubit/cubit.dart
+++ lib/shared/cubit/cubit.dart
@@ -3,12 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../modules/complaints/complaints.dart';
 import '../../modules/home/home.dart';
-import '../../modules/profile/help_center/help_center.dart';
-import '../../modules/profile/personal_information/personal_information.dart';
 import '../../modules/profile/profile.dart';
-import '../../modules/profile/protection_safety/protection_safety.dart';
-import '../../modules/profile/questions/questions.dart';
-import '../../modules/profile/settings/settings.dart';
 import '../../modules/records/records.dart';
 
 class UniversityCubit extends Cubit<UniversityStates> {
@@ -16,18 +11,12 @@ class UniversityCubit extends Cubit<UniversityStates> {
 
   static UniversityCubit get(context) => BlocProvider.of(context);
 
-  int currentIndex = 0;
+  int currentIndex = 3;
   List<Widget> screens = [
     ProfileScreen(),
-
     RecordsScreen(),
     ComplaintsScreen(),
     HomeScreen(),
-    PersonalInformation(),
-    ProtectionSafety(),
-    Questions(),
-    HelpCenter(),
-    Settings()
   ];
 
   void changeBottomNav(int index) {
